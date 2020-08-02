06ab768c00f98b21e3ef17151521b3fb05bd3987
lichen404 <f360family@gmail.com>
2020-08-02 19:13:04 +0800

    添加svg-loader;添加三个icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+
+require('icons/money.svg');
+require('icons/tags.svg');
+require('icons/statistics.svg');
+
 const NavWrapper = styled.nav`
       line-height: 24px;
       box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -10,27 +15,42 @@ const NavWrapper = styled.nav`
         >li{
         flex: 33.3%;
         text-align: center;
-        padding: 16px;
+        padding: 4px 0;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        .icon {
+        width: 24px;
+        height: 24px;
+        }
       
     }
   }
 `;
-const Nav =()=>{
+const Nav = () => {
     return (
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#tags"/>
+                    </svg>
                     <Link to="/tags">标签</Link>
                 </li>
-                <li>
+                <li><svg className="icon">
+                    <use xlinkHref="#money"/>
+                </svg>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#statistics"/>
+                    </svg>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
         </NavWrapper>
 
-    )
-}
-export default  Nav;
+    );
+};
+export default Nav;
