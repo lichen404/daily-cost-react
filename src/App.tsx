c7c50683d6254622ea4920365731e911ef7c6246
lichen404 <f360family@gmail.com>
2020-08-02 17:17:25 +0800

    完成导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,89 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
 
 
+} from 'react-router-dom';
+import styled from 'styled-components';
 
-import Button from "./components/Button";
+const Wrapper = styled.div`
+ 
+  min-height: 100vh;
+  display:flex;
+  flex-direction: column;
+  >ul {
+         display:flex;
+    >li{
+      flex: 33.3%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
+const Main = styled.div`
+  
+  flex-grow: 1;
+
+`;
+export default function App() {
+    return (
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics/>
+                        </Route>
+                        <Redirect exact from="/" to="/money"/>
+                        <Route path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+                <ul>
+                    <li>
+                        <Link to="/tags">标签</Link>
+                    </li>
+                    <li>
+                        <Link to="/money">记账</Link>
+                    </li>
+                    <li>
+                        <Link to="/statistics">统计</Link>
+                    </li>
+                </ul>
 
 
-function App() {
-  return (
-      <div>
-        <Button>你好</Button>
-      </div>
-  );
+            </Wrapper>
+        </Router>
+    );
 }
 
-export default App;
+function NoMatch() {
+    return <h2>404</h2>;
+}
+
+function Statistics() {
+    return <h2>统计页面</h2>;
+}
+
+function Tags() {
+    return <h2>标签页面</h2>;
+}
+
+function Money() {
+
+
+    return (
+        <h2>记账页面</h2>
+    );
+}
