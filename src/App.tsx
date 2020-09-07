7fc1b5c346bc3567fc0c8992516283114853bf37
lichen404 <f360family@gmail.com>
2020-09-07 23:19:42 +0800

    新建Layout组件并把三个页面变成单独的文件

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,34 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-
     Redirect,
-
-
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
 
-const Wrapper = styled.div`
- 
-  min-height: 100vh;
-  display:flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  
-  flex-grow: 1;
 
-`;
 
 export default function App() {
     return (
         <Router>
-            <Wrapper>
-                <Main>
-                    <Switch>
-                        <Route path="/tags">
-                            <Tags/>
-                        </Route>
-                        <Route path="/money">
-                            <Money/>
-                        </Route>
-                        <Route path="/statistics">
-                            <Statistics/>
-                        </Route>
-                        <Redirect exact from="/" to="/money"/>
-                        <Route path="*">
-                            <NoMatch/>
-                        </Route>
-                    </Switch>
-                </Main>
-                <Nav/>
 
+            <Switch>
+                <Route path="/tags">
+                    <Tags/>
+                </Route>
+                <Route path="/money">
+                    <Money/>
+                </Route>
+                <Route path="/statistics">
+                    <Statistics/>
+                </Route>
+                <Redirect exact from="/" to="/money"/>
+                <Route path="*">
+                    <NoMatch/>
+                </Route>
+            </Switch>
 
-            </Wrapper>
         </Router>
     );
 }
@@ -56,18 +39,6 @@ function NoMatch() {
     return <h2>404</h2>;
 }
 
-function Statistics() {
-    return <h2>统计页面</h2>;
-}
-
-function Tags() {
-    return <h2>标签页面</h2>;
-}
-
-function Money() {
 
 
-    return (
-        <h2>记账页面</h2>
-    );
-}
+
