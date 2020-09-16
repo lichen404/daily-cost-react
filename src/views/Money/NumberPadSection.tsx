2eb0e934849fa7a4ae69966cd2b4cb2d4ac297dc
lichen404 <f360family@gmail.com>
2020-09-16 23:48:30 +0800

    完成NumberPad组件的基本功能

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,58 +1,56 @@
-import styled from "styled-components";
+import React, {useState} from 'react';
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {calculateOutput} from './NumberPadSection/calculateOutput';
 
-const NumberPadSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  >.pad {
-      display: grid;
-      grid-template-rows: 64px 64px 64px 64px;
-      grid-template-columns:25% 25% 25% 25% ;
-      >button {
-        font-size: 18px;
-        border: none;
-        &.ok{
-        grid-column:4/5;
-        grid-row:3/5;
-      }
-        &.zero {
-        grid-column:1/3;
-        grid-row:4/5;
-      }
-      &:nth-child(1){
-          background-color: #f2f2f2;
+const NumberPadSection: React.FC = () => {
+    const [output, _setOutput] = useState('0');
+    const setOutput = (output: string) => {
+        if (output.length > 16) {
+            _setOutput(output.slice(0, 16));
+        } else if (output.length === 0) {
+            _setOutput('0');
+        } else {
+            _setOutput(output);
         }
-      &:nth-child(2),&:nth-child(5){
-        background-color: #e0e0e0;
-      }
-      &:nth-child(3),&:nth-child(6),&:nth-child(9){
-        background-color: #d3d3d3;
-      }
-      &:nth-child(4),&:nth-child(7),&:nth-child(10){
-        background-color: #c1c1c1;
-      }
-      &:nth-child(8),&:nth-child(11),&:nth-child(13){
-        background-color: #b8b8b8;
-      }
-      &:nth-child(12){
-        background-color:#9a9a9a;
-      }
-      &:nth-child(14){
-        background-color:#a9a9a9;
-      }
-      }
-    
-  }
-  >.output {
-    background-color: #fff;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                inset 0 5px 5px -5px  rgba(0,0,0,0.25);
-    
-    
-    
-  }
-`;
-export  default NumberPadSection;
+    };
+
+    const onClickButtonWrapper = (e: React.MouseEvent) => {
+        const text = (e.target as HTMLButtonElement).textContent;
+        if (text === null) {
+            return;
+        }
+        if (text === 'ok') {
+            // TODO
+            return;
+        }
+        if (('0123456789.'.split('').concat(['删除', '清空']).indexOf(text)) >= 0) {
+            setOutput(calculateOutput(text, output));
+        }
+
+
+    };
+    return (
+        <Wrapper>
+            <div className="output">{output}</div>
+            <div className="pad" onClick={
+                onClickButtonWrapper
+            }>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button className="ok">OK</button>
+                <button className="zero">0</button>
+                <button>.</button>
+            </div>
+        </Wrapper>
+    );
+};
+export default NumberPadSection;
