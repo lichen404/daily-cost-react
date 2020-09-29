18d45b520547ac459126d82e36dfa0e2ed21933d
lichen404 <f360family@gmail.com>
2020-09-29 23:37:35 +0800

    创建单独的 Input 组件,创建 TopBar

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,27 +1,13 @@
 import styled from "styled-components";
 import React from 'react';
+import {Input} from '../../components/Input';
 
 
 const Wrapper = styled.section`
     background-color: #f5f5f5;
     padding: 0 16px;
     font-size: 14px;
-    > label {
-    display: flex;
-    align-items: center;
-      > span {
-        margin-right: 16px;
-        white-space: nowrap;
-        
-      }
-      > input {
-          display: block;
-          width: 100%;
-          height: 72px;
-          border: 0;
-          background:none;
-        }
-    }
+
 `;
 type Props = {
     value:string,
@@ -31,12 +17,7 @@ const NoteSection:React.FC<Props> = (props)=>{
   const  note = props.value
     return (
         <Wrapper>
-            <label>
-                <span>备注</span>
-                <input type="text" placeholder="在这里添加备注" value={note} onChange={
-                    (e)=>props.onChange(e.target.value)}
-                />
-            </label>
+            <Input label="备注" type="text" value={note} onChange={(e)=>props.onChange(e.target.value)}/>
         </Wrapper>
     )
 }
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import {useTags} from '../useTags';
+import Layout from '../components/Layout';
+import Icon from '../components/icon';
+import Button from 'components/Button';
+import styled from 'styled-components';
 type Params = {
     id:string
 }
+
+const TopBar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background-color: white;
+`
 const Tag:React.FC =()=> {
     const {findTag}= useTags()
     const {id} = useParams<Params>()
     const tag =findTag(parseInt(id))
     return (
-        <span>{tag.name}</span>
+        <Layout>
+            <TopBar>
+                     <Icon name="left"/>
+                     <span>编辑标签</span>
+                <Icon/>
+            </TopBar>
+            <div>
+                <label>
+                    <span>备注</span>
+                    <input type="text" placeholder="标签名"/>
+                </label>
+            </div>
+            <Button>删除标签</Button>
+        </Layout>
+
     )
 }
 export {Tag}
